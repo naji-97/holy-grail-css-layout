27376ca582f4a676aef1d5444304ca8c9a9df2f3
Naji-97 <elbairabennaji@gmail.com>
2022-08-23 21:59:01 +0100

    finish commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleNav(){
+   var outerGrid =  document.querySelector('.outer-grid')
+  outerGrid.classList.toggle("outer-grid-expended")
+
+
+   var nav =  document.querySelector('nav')
+  nav.classList.toggle("hide-nav")
+}
